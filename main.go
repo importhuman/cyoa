18369b16c950766a185516143c5f70ff151af0b4
Ujjwal Goyal <importhuman@gmail.com>
2021-01-20 17:08:56 +0530

    Unmarshalled JSON

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	//"os"
+	//"strings"
+)
+
+type Contents struct {
+	Title   string
+	Story   []string
+	Options []map[string]string
+}
+
+func main() {
+	// open file as byte data
+	data, err := ioutil.ReadFile("gopher.json")
+	// exit if file not opened
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(data)
+
+	// gets keys as strings, values as interfaces which need to be unmarshalled further
+	var obj map[string]Contents
+
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(obj)
+}
